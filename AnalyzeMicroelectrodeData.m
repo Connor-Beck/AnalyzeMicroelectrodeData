f2fa7031e586a0e3600e4d417e4cd53c85dc9336
Connor Beck <33468526+Connor-Beck@users.noreply.github.com>
2023-07-06 10:53:39 -0600

    Add files via upload

--- AnalyzeMicroelectrodeData.m
+++ AnalyzeMicroelectrodeData.m
@@ -1,6 +1,6 @@
 %% Analyze Microelectrode signals from Multichannel Systems
 % Written and maintained by Connor Beck
-% Updated June 2023
+% Updated July 2023
 
 % Use this software to extract microelectrode array (MEA) signals from
 % multichannel systems datasets (Stored as '.h5' aka '.hdf5' file types).
@@ -10,48 +10,45 @@ clear
 clc
 addpath('Scripts\');
 
-%% User inputs
+% User inputs
 % None of these inputs are required, but can be used to tune the analysis.
 % Not all possible parameters are included here, inputs can be found in the
 % documentation of each function or in the full documentation
 
-%Key parameters
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% LOAD DATA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Parameters.Filename=[]; 
 %^Leave blank if you want to select a file with UI, 
 % otherwise include full path and file here
+[Parameters,Data] = load_MEA(Parameters);
 
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% FILTER DATA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Parameters.filter_frequencies=[300,4000]; 
 %^Used Leave blank if you want standard frequencies (300-4000 Hz), 
 % otherwise include array for High pass and Low pass
 % e.g. [400,5000] for bandpass;
 %      [,5000] for low pass only;
 %      [,] for no filter;
+[Parameters,Data] = filterElectrodes(Parameters,Data);
 
-Parameters.standard_deviation=5;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PLOT  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+plotElectrodes(Parameters,Data);
+
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%% REMOVE ELECTRODES  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Parameters.electrode_removal={};
+%^Select Electrodes to Remove,
+% Leave blank if you dont want to remove any. e.g. {}
+[Parameters,Data]=RemoveElectrodes(Parameters,Data);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% DETECT SPIKES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+Parameters.standard_deviation=4;
 %^Used to detect action potentials (falling edge).
 % Leave blank if you want classic standard deviation (7 std), 
 % otherwise set value;
+[Parameters,Data]=SpikeDetection(Parameters,Data,'Window Detection',20);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ANALYSIS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Load in data
-[Parameters,Data] = load_MEA(Parameters);
-
-% Filter Data
-[Parameters,Data] = filterElectrodes(Parameters,Data);
-
-plotElectrodes(Parameters,Data);
-
-%% After inspecting electrodes use this part of the code
-
-electrode_removal={'73','85'};
-%^Select Electrodes to Remove,
-% Leave blank if you dont want to remove any. e.g. {}
-[Parameters,Data]=RemoveElectrodes(Parameters,Data,electrode_removal);
-
-% Detect spikes
-[Parameters,Data]=SpikeDetection(Parameters,Data);
-
-%Plot Spikes
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PLOT SPIKES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure();
 scatter(Data.SpikeOutput(:,1),Data.SpikeOutput(:,2));
+set(gca, 'Ytick',1:60,'YTickLabel',Parameters.ElectrodeLabel);
